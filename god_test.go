41625691c9cd5863672af8241690ff05cd8337d0
bbp <zzffbbpp@gmail.com>
2016-03-15 09:02:13 +0800

    use testify/require

--- god_test.go
+++ god_test.go
@@ -6,10 +6,11 @@ import (
 	"time"
 
 	"github.com/golang/protobuf/proto"
+	"github.com/stretchr/testify/require"
 )
 
 func TestGod(t *testing.T) {
-	ext.AssertNoError(t,
+	require.NoError(t,
 		Start("amqp://guest:guest@localhost:5672/", 0, ext.RandomUint64()),
 		"start god")
 
@@ -17,26 +18,26 @@ func TestGod(t *testing.T) {
 	var err error
 
 	consumer, err = NewSession()
-	ext.AssertNoError(t, err, "new consumer")
+	require.NoError(t, err, "new consumer")
 
 	exchange := "god.test"
 	routingKeyType := ext.RandomUint16()
 	routingKey := ext.RandomUint64()
 
 	q, err := consumer.Subscribe(exchange, routingKeyType, routingKey)
-	ext.AssertNoError(t, err, "pull msgs")
+	require.NoError(t, err, "pull msgs")
 	ci := int64(0)
 	go consumer.Handle(q,
 		func(method string, msg proto.Message) error {
-			ext.CheckEqual(t, method, "Test")
+			require.Equal(t, method, "Test")
 			test := msg.(*Test)
-			ext.CheckEqual(t, test.Count, ci)
+			require.Equal(t, test.Count, ci)
 			ci++
 			return nil
 		})
 
 	producer, err = NewSession()
-	ext.AssertNoError(t, err, "new producer")
+	require.NoError(t, err, "new producer")
 
 	for i := int64(0); i < 1000; i++ {
 		var test Test
@@ -44,7 +45,7 @@ func TestGod(t *testing.T) {
 		err = producer.Post(exchange,
 			routingKeyType, routingKey,
 			"Test", &test)
-		ext.AssertNoError(t, err, "post")
+		require.NoError(t, err, "post")
 	}
 
 	time.Sleep(time.Millisecond * 100)
